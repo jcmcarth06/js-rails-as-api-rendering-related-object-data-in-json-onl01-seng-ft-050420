44973b1c2a3f69875b19fafac0ceba0eb018f32e
Joshua McArthur <jcmcarth06@gmail.com>
2020-08-16 00:22:26 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -5,8 +5,8 @@ class SightingsController < ApplicationController
   end
 
   def index
-  sightings = Sighting.all
-  render json: sightings, include: [:bird, :location]
-end
+    sightings = Sighting.all
+    render json: sightings, include: [:bird, :location]
+  end
 
 end
